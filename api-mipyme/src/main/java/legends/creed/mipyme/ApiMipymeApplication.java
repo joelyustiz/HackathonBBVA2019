a82a2504ec78d15430084c056954dd3052b72f3d
Rodrigo Noe Trejo Guerra <rodrigo.noe@hotmail.com>
2019-09-07 16:21:13 -0500

    init proyect

---
+++ api-mipyme/src/main/java/legends/creed/mipyme/ApiMipymeApplication.java
@@ -0,0 +1,13 @@
+package legends.creed.mipyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMipymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMipymeApplication.class, args);
+	}
+
+}
