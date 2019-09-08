7fad50552694cba6deb716ee6625903e89cb8c05
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-08 00:42:03 -0500

    Updating App Content

--- app-mipyme/src/home/App.js
+++ app-mipyme/src/home/App.js
@@ -1,9 +1,15 @@
 //@Author William E. Velázquez A. - info@williamvelazquez.com
 import React, { Component } from 'react';
 
-import CubeLoader from "../components/CubeLoader";
-
 import '../libs/animate.css';
+import '../libs/bootstrap.min.css';
+
+import './app.css';
+
+import CubeLoader from "../components/CubeLoader";
+import ContentLayout from '../components/ContentLayout';
+import LabelInput from '../components/LabelInput';
+import Button from '../components/Button';
 // import * as Constants from 'Constants/app';
 
 class App extends Component {
@@ -11,7 +17,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state =  {
-      loading: true
+      loading: false,
+      clientId: 0
     };
   }
 
@@ -19,11 +26,36 @@ class App extends Component {
     this.setState({ loading: value }, ()=>{callback && callback()});
   };
 
+  handleChange = event => {
+    // console.log(`value-->${event.target.value} | id-->${event.target.id}`);
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSearchClient = (id) => {
+    console.log('id--->', id);
+  }
+
   render() {
+    const {clientId}=this.state;
     return (
-      <React.Fragment>
+      <ContentLayout>
+        <section className="search-container">
+          <LabelInput 
+            id="clientId"
+            label="Ingresa la clave del cliente:"
+            value={clientId}
+            placeholder="Clave de Cliente"
+            handleChange={this.handleChange}
+          />
+          <Button 
+            text="Buscar"
+            handleButtonClick={()=>this.handleSearchClient(clientId)}
+          />
+        </section>
         {this.state.loading && <CubeLoader />}
-      </React.Fragment>
+      </ContentLayout>
     );
   }
 }
