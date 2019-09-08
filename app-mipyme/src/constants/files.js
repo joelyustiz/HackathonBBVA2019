af4f46467c5d28346b560caf83f446965b81df12
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-08 00:41:07 -0500

    Adding Logo image and Bootstrap styles

--- app-mipyme/src/constants/files.js
+++ app-mipyme/src/constants/files.js
@@ -1,2 +1,3 @@
 //@Author William E. Velázquez A. - info@williamvelazquez.com
 export const USER_PICTURE = require("../images/profile.png");
+export const BBVA_LOGO = require("../images/Logo_BBVA.png");
